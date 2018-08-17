d964988950d230959acf2bce2510ee88a47be873
Liam <l.mccolgan.connect@gmail.com>
2018-08-17 13:40:18 +0100

    17/08/18 added js to hide header-container on scroll for viewport > 1000px

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,15 +4,24 @@ $(document).ready(function(){
     linkSwitching();
   });
 
- 
+ // Checking to see if screen width matches media query for desktop header
+ let screenWidth = window.matchMedia( "(min-width: 1000px)" );
+
   // Smooth Scrolling
   var scrollLink = $('.scroll');
   scrollLink.click(function(e){
     e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top - $('.header-container').outerHeight()
-    }, 1500);
-  });
+    if (screenWidth.matches){
+      $('body,html').animate({
+        //scrollTop: $(this.hash).offset().top - $('#header-container').outerHeight()
+        scrollTop: $(this.hash).offset().top
+      }, 1500);
+    } else{
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top - $('#header-container').outerHeight()
+      }, 1500);
+    }
+    });
     // Nav-Icon Animation
   $('#nav-icon').click(function(){
     $('.site-nav').toggleClass('site-nav--open', 400);
@@ -40,5 +49,22 @@ $(document).ready(function(){
     var wScroll = $(window).scrollTop();
     $('.showcase-bg').css('top',(wScroll * 0.04)+'em');
   }
+
+  // Hiding header on scroll
+  let prevScrollPos = window.pageYOffset;
+  
+  
+  if (screenWidth.matches){
+       window.onscroll = function(){
+          let currentScrollPos = window.pageYOffset;
+          if (prevScrollPos > currentScrollPos){
+              document.getElementById("header-container").style.top = "0";
+          } else{
+              document.getElementById("header-container").style.top = "-90px";
+          }
+      
+          prevScrollPos = currentScrollPos;
+          }
+      }
 
 });
